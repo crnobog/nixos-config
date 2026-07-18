8618f106087eab5027fa8e10ce4d7478a2e58001
Robert Millar <robert.millar@cantab.net>
2026-07-18 10:36:54 -0700

    Added tealdeer

--- users/rob/default.nix
+++ users/rob/default.nix
@@ -5,16 +5,17 @@
   pkgs-pi,
   pkgs-unstable,
   ...
-}:
-{
+}: {
   users.users.rob = {
     isNormalUser = true;
     description = "Robert Millar";
     extraGroups = [
       "networkmanager"
       "wheel"
-    ]; # sudo
+    ];
     packages = with pkgs; [
+      tealdeer
+
       pkgs-pi.coding-agent
     ];
   };
