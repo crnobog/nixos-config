dfc7c6f189da738a6c691ccc42ea8c7f7c70b36d
Robert Millar <robert.millar@cantab.net>
2026-08-16 11:37:09 -0700

    Remove old key from meshify

--- modules/common/inventory.nix
+++ modules/common/inventory.nix
@@ -7,7 +7,6 @@
   meshify = {
     hostName = "meshify";
     ip = "192.168.0.126";
-    publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAJEMzg5GZ9mz1x8ujXPXgD03Y37eBT4I7HFE78HB418";
   };
   sylph = {
     hostName = "sylph";
